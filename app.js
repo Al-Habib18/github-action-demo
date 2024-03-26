ff77d0cffac341bbb03f8d9ab3245b3ec8d5ef3e
Al-Habib18 <ah18bd@gamil.com>
2024-03-26 22:08:29 +0600

    imlement health route

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/health", (req, res) => {
+    res.status(200).send("Hello World!");
 });
 
 module.exports = app;
