2ffd8921cf7b69578fdf7497d26088464f58ea9c
Al-Habib18 <ah18bd@gamil.com>
2024-03-26 22:21:27 +0600

    implement global error handler route

--- app.js
+++ app.js
@@ -8,4 +8,14 @@ app.get("/health", (req, res) => {
     res.status(200).send("Hello World!");
 });
 
+// == Global error-handler == //
+app.use((err, req, res, next) => {
+    const errObj = {
+        message: err?.message || "Someting went wrong",
+        status: err?.status || 500,
+    };
+    console.error(err);
+    res.status(errObj.status).json(errObj);
+});
+
 module.exports = app;
